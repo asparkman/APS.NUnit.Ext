df31c4e9305cc44ce297384147c9a2e074c98496
asparkman <alex.sparkman@iinteractive.com>
2014-05-19 07:09:10 -0500

    - Updated description of TestWithConnections property.

--- APS.NUnit.Ext/APS.NUnit.Ext/BaseProvider.cs
+++ APS.NUnit.Ext/APS.NUnit.Ext/BaseProvider.cs
@@ -20,6 +20,8 @@ namespace APS.NUnit.Ext
         
         /// <summary>
         /// Returns the TestWithConnections app setting value.
+        /// 
+        /// It will return false if it is not set.
         /// </summary>
         public static bool TestWithConnections
         {
